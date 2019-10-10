b74dfc263f95ca8870fc7d0603439facb7538ec4
Gerd Muehlberger <gerd.muehlberger96@gmail.com>
2019-10-10 23:15:15 +0200

    added summon functionality aswell as music player

--- pahiebot.py
+++ pahiebot.py
@@ -1,23 +1,159 @@
 import os
 import discord
+from discord.ext import commands
+from discord.utils import get
+import random
+import youtube_dl
+
+
 from dotenv import load_dotenv
 
 #load_dotenv()
 token = 'NjMxNTgzODMzMDU3Mzk0NzA3.XZ4-vA.i4ZlimCWHkfVYkM4JzDF8H-RNVU'
+bot = commands.Bot(command_prefix='!')
 
-client = discord.Client()
 
-@client.event
+#######################################################################
+###############     COMMANDS SECTION     ##############################
+#######################################################################
+
+#
+# let the bot join the voice channel of the user who called it.
+#
+@bot.command(pass_context=True)
+async def join(ctx):
+    global voice
+    channel = ctx.message.author.voice.channel
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+
+    await ctx.send(f'Pahie joined {channel}')
+
+
+#
+# send the bot out of the voice channel again
+#
+@bot.command(pass_context=True)
+async def leave(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.disconnect()
+        print(f'Bot left {channel}')
+        await ctx.send(f'Pahie is pissed and left {channel}')
+    else:
+        print("bot was told to leave but wasnt in one...")
+        await ctx.send("Pahie could'nt be found in any voice channel...")
+
+#
+#print that the bot is ready to make sure that it actually logged on
+#
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected!')
+    print(f'{bot.user} has connected!')
 
-@client.event
+#######################################################################
+##################     MESSAGING SECTION     ##########################
+#######################################################################
+
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return "pahie..."
 
+    #
+    # this fixes the command issue, its just a quickfix though.
+    # bug : https://stackoverflow.com/questions/49331096/why-does-on-message-stop-commands-from-working
+    #
+    if message.content.startswith('!join'):
+        await bot.process_commands(message)
+
+    if message.content.startswith('!leave'):
+        await bot.process_commands(message)
+
+    if message.content.startswith('!play'):
+        await bot.process_commands(message)
+
     if "pahie" in message.content:
-        response = ("pahie...")
+        response = ("Pahie...")
         await message.channel.send(response)
 
-client.run(token)
+    if "pahie" and "story" in message.content:
+
+        def getSentence():
+            sentence_count = random.randint(5, 15)
+            punctuation_marks = [".", ",", "?", "!"]
+            words = ""
+            story = ""
+
+            for x in range(sentence_count):
+                for y in range(random.randint(1, 8)):
+                    words = words + " " + "Pahie"
+                words = words + random.choice(punctuation_marks)
+            story = story + words
+
+            return story
+
+        response = getSentence()
+
+        await message.channel.send(response)
+
+
+
+#######################################################################
+################      AUDIO PLAYING SECTION     #######################
+#######################################################################
+
+#
+# this needs to be altered. it works but i only want a temp file that plays when a link is pasted
+#
+
+@bot.command(pass_context=True)
+async def play(ctx, url: str):
+    song = os.path.isfile("song.mp3")
+    try:
+        if song:
+            os.remove("song.mp3")
+            print("removed old song")
+    except PermissionError:
+        print("song cant be removed when played!")
+        await ctx.send("Music is already playing.")
+        return
+
+    await ctx.send("getting rdy")
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    ydl_options = {
+        'format': 'bestaudio/best',
+        'postprocessors':[{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_options) as ydl:
+        print("downloading audio now")
+        ydl.download([url])
+
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            name = file
+            print(f"renamed file to {file}")
+            os.rename(file, "song.mp3")
+
+    voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: print(f"{name} has finished playing"))
+    voice.source = discord.PCMVolumeTransformer(voice.source)
+    voice.source.volume = 0.07
+
+    new_name = name.rsplit("-", 2)
+    await ctx.send(f"playing: {new_name}")
+    print("playing")
+
+
+bot.run(token)
