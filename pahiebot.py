8d065bf63e83e994764924512f583e52978b5014
Gerd Muehlberger <gerd.muehlberger96@gmail.com>
2020-03-15 21:31:57 +0100

    deleted a few functions for a stable version that could be run permanently

--- pahiebot.py
+++ pahiebot.py
@@ -3,22 +3,29 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 import random
+from pathlib import Path
+import json
+import logging
 import time
 import youtube_dl
 
+currentWorkingDirectory = Path(__file__).parents[0]
+currentWorkingDirectory = str(currentWorkingDirectory)
+print(currentWorkingDirectory)
 
-from dotenv import load_dotenv
+# Authorization
+secretFile = json.load(open(currentWorkingDirectory+'/config/secrets.json'))
+bot = commands.Bot(command_prefix='!', case_insensitive=True)
+bot.config_token = secretFile['token']
+logging.basicConfig(level=logging.INFO)
 
-#load_dotenv()
-token = 'NjMxNTgzODMzMDU3Mzk0NzA3.XZ4-vA.i4ZlimCWHkfVYkM4JzDF8H-RNVU'
-bot = commands.Bot(command_prefix='!')
 
 #
 #print that the bot is ready to make sure that it actually logged on
 #
 @bot.event
 async def on_ready():
-    print(f'{bot.user} has connected!')
+    print(f'{bot.user} has connected! id={bot.user.id}')
 
 
 #######################################################################
@@ -38,7 +45,6 @@ async def summonpahie(ctx):
         await voice.move_to(channel)
     else:
         voice = await channel.connect()
-
     await ctx.send(f'Pahie joined {channel}')
 
 
@@ -58,7 +64,6 @@ async def kickpahie(ctx):
         print("bot was told to leave but wasnt in one...")
         await ctx.send("Pahie could'nt be found in any voice channel...")
 
-
 #
 # send list of available commands to textchat
 #
@@ -67,97 +72,7 @@ async def helpmepahie(ctx):
     await ctx.send("!summonpahie: summons Pahie into your channel.\n"
                    "!kickpahie: kicks Pahie out of your channel.\n"
                    "!bobquote: Pahie plays Spongebobquote (Requires summoning to a channel first). \n"
-                   "!unleashpahie: Pahie runs away and appears randomly in your channel at some point. "
-                   "(He will not listen to commands while unleashed.)\n"
-                   "!leashpahie: Pahie starts following your commands again.")
-
-
-#
-# once the bot is ready, run a function thats called at random times and
-# lets the bot join into a random channel, play a soundfile and leave again
-#
-
-pahie_is_unleashed = False
-
-def globallyChange():
-    global pahie_is_unleashed
-    pahie_is_unleashed = not pahie_is_unleashed
-
-
-async def play_pahie_sound(ctx):
-    try:
-        if voice is not None:
-            voice.play(discord.FFmpegPCMAudio(f"bobquotes/1.mp3"),
-                       after=lambda e: "song finished playing")
-            voice.source = discord.PCMVolumeTransformer(voice.source)
-            voice.source.volume = 0.07
-        else:
-            await ctx.send("Pahie is not here!")
-
-    except Exception as e:
-        print(e)
-        await ctx.send("Wait until the current quote is finished!")
-        return
-
-
-
-@bot.command(pass_context=True)
-async def unleashpahie(ctx):
-
-    await ctx.send("You see Pahie running away in the distance...\n"
-                   "He will not listen to any commands until he visits or you caught him again.\n"
-                   "Type !leashpahie to catch him next time hes visiting.")
-
-    globallyChange()
-    starttime = time.time()
-
-    while pahie_is_unleashed is True:
-        #
-        # bot joins the channel
-        #
-        random_timestamp = random.randint(20, 60)
-
-        global voice
-        channel = ctx.message.author.voice.channel
-        voice = get(bot.voice_clients, guild=ctx.guild)
-
-        if voice and voice.is_connected():
-            await voice.move_to(channel)
-        else:
-            voice = await channel.connect()
-
-        #await ctx.send(f'Pahie runs towards {ctx.message.author}!')
-
-        #
-        # bot plays sound and waits for the soundfile to finish
-        #
-
-        await play_pahie_sound(ctx)
-        time.sleep(6)
-
-        #
-        # bot leaves channel again and waits for repeat
-        #
-
-
-        if voice and voice.is_connected():
-            await voice.disconnect()
-            #await ctx.send(f'Pahie run away from {ctx.message.author} again.')
-        else:
-            await ctx.send("Pahie could'nt be found in any voice channel...")
-
-        time.sleep(random_timestamp - ((time.time() - starttime) % random_timestamp))
-
-#
-# this should stop the bot from joining again
-#
-@bot.command(pass_context=True)
-async def leashpahie(ctx):
-    if pahie_is_unleashed is True:
-        globallyChange()
-        await ctx.send(f'You leashed Pahie again...')
-    else:
-        await ctx.send(f'Pahie has not been unleashed yet.')
+                   )
 
 
 #######################################################################
@@ -183,46 +98,8 @@ async def on_message(message):
     if message.content.startswith('!helpmepahie'):
         await bot.process_commands(message)
 
-    if message.content.startswith('!unleashpahie'):
-        await bot.process_commands(message)
-
-    if message.content.startswith('!leashpahie'):
-        await bot.process_commands(message)
-
     if message.content.startswith('!bobquote'):
         await bot.process_commands(message)
-
-
-    #
-    # bot answers with "pahie" everytime he is mentioned
-    #
-    if "pahie" in message.content:
-        response = ("Pahie...")
-        await message.channel.send(response)
-
-    #
-    # bot tells "story"
-    #
-    if "pahie" and "story" in message.content:
-
-        def getSentence():
-            sentence_count = random.randint(5, 15)
-            punctuation_marks = [".", ",", "?", "!"]
-            words = ""
-            story = ""
-
-            for x in range(sentence_count):
-                for y in range(random.randint(1, 8)):
-                    words = words + " " + "Pahie"
-                words = words + random.choice(punctuation_marks)
-            story = story + words
-
-            return story
-
-        response = getSentence()
-
-        await message.channel.send(response)
-
 
 
 #######################################################################
@@ -259,4 +136,4 @@ async def bobquote(ctx):
 ###################      LEVELING SECTION     #########################
 #######################################################################
 
-bot.run(token)
+bot.run(bot.config_token)
