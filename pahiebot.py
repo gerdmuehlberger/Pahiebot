891f83f4eb2e018cc4229dcb5ec2edc730268f50
zoidgerd <gerd.muehlberger96@gmail.com>
2020-04-26 11:28:35 +0200

    added proper errorhandling for missing argument errors in commands

--- pahiebot.py
+++ pahiebot.py
@@ -6,11 +6,13 @@ from discord.utils import get
 import random
 from pathlib import Path
 import json
+import sys
 import logging
 import time
 import youtube_dl
 import praw
 import itertools
+import traceback
 
 
 #######################################################################
@@ -27,8 +29,57 @@ secretFile = json.load(open(currentWorkingDirectory+'/config/secrets.json'))
 bot = commands.Bot(command_prefix='!', case_insensitive=True)
 bot.config_token = secretFile['token']
 
-#activate discord logs by uncommenting this
-#logging.basicConfig(level=logging.INFO)
+
+
+#######################################################################
+#################     ERRORHANDLING      ##############################
+#######################################################################
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    """The event triggered when an error is raised while invoking a command.
+    ctx   : Context
+    error : Exception"""
+
+    # This prevents any commands with local handlers being handled here in on_command_error.
+    if hasattr(ctx.command, 'on_error'):
+        return
+
+    ignored = (commands.CommandNotFound, commands.UserInputError)
+
+    # Allows us to check for original exceptions raised and sent to CommandInvokeError.
+    # If nothing is found. We keep the exception passed to on_command_error.
+    error = getattr(error, 'original', error)
+
+    if isinstance(error, commands.MissingRequiredArgument):
+        return await ctx.send("Seems like you forgot to enter a required argument for that command! (Type !helpmepahie for more infos.)")
+
+    elif isinstance(error, commands.DisabledCommand):
+        return await ctx.send(f'{ctx.command} has been disabled.')
+
+    elif isinstance(error, commands.NoPrivateMessage):
+        try:
+            return await ctx.author.send(f'{ctx.command} can not be used in Private Messages.')
+        except:
+            pass
+
+    # For this error example we check to see where it came from...
+    elif isinstance(error, commands.BadArgument):
+        if ctx.command.qualified_name == 'tag list':  # Check if the command being invoked is 'tag list'
+            return await ctx.send('I could not find that member. Please try again.')
+
+    # Anything in ignored will return and prevent anything happening.
+    # This should be checked every now and then to maybe update the errorhandling
+    elif isinstance(error, ignored):
+        print(f"Unknown error to the Pahiebot errorhandling occured: {error}")
+        return
+
+
+    # All other Errors not returned come here... And we can just print the default TraceBack.
+    print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+    traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+
 
 
 #
@@ -37,6 +88,8 @@ bot.config_token = secretFile['token']
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected! id={bot.user.id}')
+
+
 
 
 #######################################################################
@@ -121,6 +174,7 @@ async def sendpahie(ctx, channelname):
 
         else:
             await ctx.send(f"There is no voicechannel called \'{channelname}\' on this server!")
+
 
     except AttributeError:
         print("didnt work.")
