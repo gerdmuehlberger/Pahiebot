33c5eca6876b3e939c19aace5178f3aee9b339bf
zoidgerd <gerd.muehlberger96@gmail.com>
2021-02-17 21:10:17 +0100

    removed some files

--- setup/Commands.py
+++ setup/Commands.py
@@ -318,7 +318,7 @@ class Commands:
         ##################################################################
         @botObject.command(pass_context=True)
         async def availableaudio(ctx, audiocategory):
-            supportedAudioCategories = ['atv', 'spongebob', 'misc', 'smoove']
+            supportedAudioCategories = ['atv', 'misc', 'smoove']
 
             if audiocategory in supportedAudioCategories:
                 audioFilePath = f"./audio/audiofiles/{audiocategory}/"
@@ -349,7 +349,7 @@ class Commands:
         ##################################################################
         @botObject.command(pass_context=True)
         async def play(ctx, audiocategory, audiofilename):
-            supportedAudioCategories = ['atv', 'spongebob', 'misc', 'smoove']
+            supportedAudioCategories = ['atv', 'misc', 'smoove']
 
             try:
                 channelNameOfMessageAuthor = ctx.message.author.voice.channel
